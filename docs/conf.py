07d53d51272bb01c1137ecbc7de8b5ca81b06143
wyq24wl <122544328+wyq24wl@users.noreply.github.com>
2024-02-27 14:04:19 -0500

    add documentation through sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 
 project = 'pygsfit'
 copyright = '2024, Yuqian Wei'
-author = 'Yuqian Wei'
+author = 'Bin Chen, Sijie Yu, Yuqian Wei'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -25,3 +25,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../pygsfit'))
